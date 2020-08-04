0e37d74b9d0038f51588a78dbca9d72315f974e8
tazv2 <tjkhan22@outlook.com>
2020-08-04 22:10:04 +0600

    check file duration in mins

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from mutagen.mp4 import MP4
+
+all_files = os.listdir()
+
+
+for i in all_files:
+	mp4 = 'mp4'
+	if os.path.abspath(i).endswith(mp4):
+		detalis = MP4(os.path.abspath(i))
+		print(f"Duration of {i} is{detalis.info.length/60} mins")
